2e8dfc170aebf33948abeee2d0827712a69fecdd
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2022-05-29 11:24:54 +0600

    project setup,folder name change

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -16,10 +16,11 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { MdDashboard, MdHelp } from "react-icons/md";
-import { RiSettings4Fill, RiAccountBoxLine } from "react-icons/ri";
+import { RiSettings4Fill } from "react-icons/ri";
 import { GoProject } from "react-icons/go";
 import { useNavigate } from "react-router-dom";
 import { GiBatteryPackAlt } from "react-icons/gi";
+import { AiOutlineRise } from "react-icons/ai";
 import { BiLogOutCircle } from "react-icons/bi";
 import { FaRegEnvelope } from "react-icons/fa";
 import { HiHome, HiOutlineDocumentReport } from "react-icons/hi";
@@ -168,7 +169,7 @@ function Home() {
               navigate("/");
             }}
           >
-            Investor
+            Admin
           </Typography>
           {/* <Box sx={{ flexGrow: 1 }} /> */}
 
@@ -270,13 +271,14 @@ function Home() {
         <List>
           {[
             "Home", //0
-            "Investor(Dashboard)", //1
+            "Dashboard", //1
             "Inbox", //2
-            "Investor(all projects/packages)", //3
-            "Report", //4
-            "Investor Account", //5
-            "Help Center", //6
-            "Settings", //7
+            "Project", //3
+            "Rate", //4
+            "Investor", //5
+            "Suppliers", //6
+            "Support", //7
+            "Accounts", //8
           ].map((text, index) => (
             <ListItem
               Button
@@ -401,7 +403,7 @@ function Home() {
                     }
                   >
                     {index === 5 && (
-                      <RiAccountBoxLine
+                      <AiOutlineRise
                         style={{
                           fontSize: "1.5rem",
                         }}
